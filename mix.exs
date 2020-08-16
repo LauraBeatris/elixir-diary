b2076fd53b6630efdf497b0018cc468f7976c8a0
Laura <laurabeatriserafim@gmail.com>
2020-08-16 09:46:39 -0300

    chore(setup): install dependencies

---
+++ mix.exs
@@ -0,0 +1,32 @@
+defmodule Diary.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :diary,
+      version: "0.1.0",
+      elixir: "~> 1.10",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  defp deps do
+    [
+      # Testing
+      {:credo, "~> 1.4", only: [:dev, :test], runtime: false},
+      {:mix_test_watch, "~> 1.0", only: :dev, runtime: false},
+      {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
+
+      # Utils
+      {:poison, "~> 3.1"},
+      {:timex, "~> 3.5"}
+    ]
+  end
+end
