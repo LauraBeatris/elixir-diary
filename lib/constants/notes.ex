a1ea82303329d570753705af78bcd4c4f3e2f9f6
Laura <laurabeatriserafim@gmail.com>
2020-08-16 14:08:34 -0300

    docs: remove app stuff

--- lib/constants/notes.ex
+++
@@ -1,9 +0,0 @@
-defmodule Notes.Constants do
-  @moduledoc """
-    Notes constants
-  """
-
-  defmacro notes_filesystem_path do
-    "notes"
-  end
-end
--- test/diary_test.exs
+++
@@ -1,9 +0,0 @@
-defmodule DiaryTest do
-  use ExUnit.Case
-
-  test "should create a note with a valid value" do
-    note = Diary.create_note("A valid note")
-
-    assert note == :ok
-  end
-end
