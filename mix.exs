2538240428e5483bbb887e602b3137093a85012d
Laura <laurabeatriserafim@gmail.com>
2020-08-16 12:28:39 -0300

    chore: add dialyzer to project config

--- mix.exs
+++ mix.exs
@@ -7,7 +7,11 @@ defmodule Diary.MixProject do
       version: "0.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      dialyzer: [
+        plt_add_apps: [:ex_unit, :mix],
+        plt_file: {:no_warn, "priv/plts/dialyzer.plt"},
+      ],
     ]
   end
 
